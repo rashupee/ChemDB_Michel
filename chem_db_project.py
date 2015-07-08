a8e543e8634fd93528e883229ab4d877e4170e85
Alfred Cauthen <rashupee@gmail.com>
2015-07-07 21:43:02 -0700

    Passive error handling added.

--- chem_db_project.py
+++ chem_db_project.py
@@ -11,20 +11,19 @@ CMGFileName = 'ChemProjTestData.csv'
 master = open(CMGFileName, 'r')
 
 for line in master:
-	# print line
-	# print type(line)
-	# print line.split(',',2)
 	CMGName = line.split(',',2)[0]
-	if CMGName != '':
+	if CMGName and CMGName != '':
 		SmilesParam = line.split(',',2)[1]
-		findings = open('%s.csv'% CMGName, 'a')
+		try:
+			nym_string = str(get_synonyms(SmilesParam,'smiles', searchtype='substructure'))
+			CASRN_list = re.findall(r'\d{2,7}-\d\d-\d', nym_string)
+			findings = open('%s.csv'% CMGName, 'a')
+			for nym in CASRN_list:
+				findings.write(CMGName + ',' + nym + '\n')
+		except Exception as e:
+			findings = open('%s_error.txt'% CMGName, 'a')
+			findings.write(CMGName + ": something is wrong.\nError message is: " + e.message)
 		
-		nym_string = str(get_synonyms(SmilesParam,'smiles', searchtype='substructure'))
-		CASRN_list = re.findall(r'\d{2,7}-\d\d-\d', nym_string)
-
-		for nym in CASRN_list:
-			findings.write(CMGName + ',' + nym + '\n')
-
 		findings.close()
 
 
