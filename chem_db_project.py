2062743fbe32fbdf2abc85f1512e55fa2918bb23
Alfred Cauthen <rashupee@gmail.com>
2015-07-22 22:49:39 -0700

    created a batched system to try to avoid timeout

--- chem_db_project.py
+++ chem_db_project.py
@@ -14,22 +14,51 @@ CMGFileName = 'PracticeData.csv'
 
 master = open(CMGFileName, 'r')
 
+def batchIndexes(cids):
+	""" Returns list of indicies to void timeout with pcp.get_synonyms """
+	max_batch = 1000
+	list_size = len(cids)
+	batches = int(len(cids)/max_batch) + 1
+	remainder = list_size % max_batch
+	begin = 0
+	end = 0
+	batch_indexes = []
+	while index < batches:
+		end = begin + max_batch - 1
+		batch_indexes.append((begin,end))
+		begin = end
+		index += 1
+
+	return batch_indexes
+
 for line in master:
 	CMGName = line.split(',',2)[0]
 	if CMGName and CMGName != '':
 		smiles_param = line.split(',',2)[1]
-		cids = pcp.get_cids(smiles_param, 'smiles', searchtype='substructure')
-		cas_rns = []
-		results = pcp.get_synonyms(cids)
-		for result in results:
-			for syn in result.get('Synonym', []):
-				match = re.match('(\d{2,7}-\d\d-\d)', syn)
-	    		if match:
-	        		cas_rns.append(match.group(1))
-		findings = open('Results/%s.csv'% CMGName, 'a')
-		for element in cas_rns:
-			findings.write(CMGName + ',' + element + '\n')
-		
+		print "Trying pcp.get_cids method with " + smiles_param + "..."
+		try:
+			cids = pcp.get_cids(smiles_param, 'smiles', searchtype='substructure')
+			batch_indexes = batchIndexes(cids)
+			findings = open('Results/%s.csv'% CMGName, 'a')
+			for index in batch_indexes:
+				cas_rns = []
+				print "Processing pcp.get_synonyms with cids batch ", index
+				results = pcp.get_synonyms(cids)
+				print "Finding CASRN matches in the synonyms ..."
+				for result in results:
+					for syn in result.get('Synonym', []):
+						match = re.match('(\d{2,7}-\d\d-\d)', syn)
+			    		if match:
+			        		cas_rns.append(match.group(1))
+				print "Writing results to file ..."
+				for element in cas_rns:
+					findings.write(CMGName + ',' + element + '\n')
+		except Exception as e:
+			print "Checking smiles parameter " + smiles_param + " throws error:"
+			print e.message
+			findings = open('Results/%s_error.txt'% CMGName, 'a')
+			findings.write(CMGName + ": something is wrong.\nError message is: " + e.message + '\n')
+
 		findings.close()
 
 master.close()
